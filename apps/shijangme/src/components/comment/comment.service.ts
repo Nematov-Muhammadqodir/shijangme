8beaea7925543ee267dc99883cb07b2287489b8d
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-07-27 08:38:56 +0900

    feat: develop getComments graphQL API

--- apps/shijangme/src/components/comment/comment.service.ts
+++ apps/shijangme/src/components/comment/comment.service.ts
@@ -1,14 +1,19 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { MemberService } from '../member/member.service';
 import { ProductService } from '../product/product.service';
 import { BoardArticleService } from '../board-article/board-article.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
+import {
+  CommentInput,
+  CommentsInquiry,
+} from '../../libs/dto/comment/comment.input';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
-import { Message } from '../../libs/enums/common.enum';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { T } from '../../libs/types/common';
+import { lookupMemberGeneral } from '../../libs/config';
 
 @Injectable()
 export class CommentService {
@@ -85,5 +90,41 @@ export class CommentService {
       throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
     return updatedComment;
+  }
+
+  public async getComments(
+    memberId: ObjectId,
+    input: CommentsInquiry,
+  ): Promise<Comments> {
+    const match: T = {
+      commentRefId: input.search.commentRefId,
+      commentStatus: CommentStatus.ACTIVE,
+    };
+    const sort: T = {
+      [input.sort ?? 'createdAt']: input.direction ?? Direction.DESC,
+    };
+
+    const result = await this.commentModel.aggregate([
+      { $match: match },
+      { $sort: sort },
+      {
+        $facet: {
+          list: [
+            { $skip: (input.page - 1) * input.limit },
+            { $limit: input.limit },
+            lookupMemberGeneral,
+            { $unwind: '$memberData' },
+          ],
+          metaCounter: [{ $count: 'total' }],
+        },
+      },
+    ]);
+
+    if (!result.length)
+      throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    console.log('getComments result', result[0]);
+
+    return result[0];
   }
 }
