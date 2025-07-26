eacc3197e8c3ae9e8aa65743377fb82c8870a878
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-07-27 07:57:56 +0900

    feat: develop createComment graphQL API

--- apps/shijangme/src/components/comment/comment.resolver.ts
+++ apps/shijangme/src/components/comment/comment.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Comment } from '../../libs/dto/comment/comment';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => Comment)
+  public async createComment(
+    @Args('input') input: CommentInput,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Comment> {
+    console.log('Mutation createComment');
+    return await this.commentService.createComment(memberId, input);
+  }
 }
--- apps/shijangme/src/components/comment/comment.service.ts
+++ apps/shijangme/src/components/comment/comment.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Comment } from '../../libs/dto/comment/comment';
 import { MemberService } from '../member/member.service';
 import { ProductService } from '../product/product.service';
 import { BoardArticleService } from '../board-article/board-article.service';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentGroup } from '../../libs/enums/comment.enum';
 
 @Injectable()
 export class CommentService {
@@ -14,4 +16,50 @@ export class CommentService {
     private readonly productService: ProductService,
     private readonly boardArticleService: BoardArticleService,
   ) {}
+
+  public async createComment(
+    memberId: ObjectId,
+    input: CommentInput,
+  ): Promise<Comment> {
+    input.memberId = memberId;
+
+    let result = null;
+
+    try {
+      result = await this.commentModel.create(input);
+    } catch (error) {
+      console.log('CreateComment Error', error);
+      throw new InternalServerErrorException(error.message);
+    }
+
+    switch (input.commentGroup) {
+      case CommentGroup.ARTICLE:
+        await this.boardArticleService.boardArticleStatsEditor({
+          _id: input.commentRefId,
+          targetKey: 'articleComments',
+          modifier: 1,
+        });
+        break;
+
+      case CommentGroup.MEMBER:
+        await this.memberService.memberStatsEditor({
+          _id: input.commentRefId,
+          targetKey: 'memberComments',
+          modifier: 1,
+        });
+        break;
+
+      case CommentGroup.PRODUCT:
+        await this.productService.productStatsEditor({
+          _id: input.commentRefId,
+          targetKey: 'productComments',
+          modifier: 1,
+        });
+        break;
+    }
+
+    console.log('Comment result', result);
+
+    return result;
+  }
 }
