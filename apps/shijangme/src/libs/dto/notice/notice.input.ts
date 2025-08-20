0b864e917f6098e6f562475804f4bbb93032c5a4
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-20 13:38:51 +0900

    feat: initail setup for notice

---
+++ apps/shijangme/src/libs/dto/notice/notice.input.ts
@@ -0,0 +1,27 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, Length } from 'class-validator';
+import { NoticeCategory, NoticeStatus } from '../../enums/notice.enum';
+import { ObjectId } from 'mongoose';
+
+@InputType()
+export class MemberInput {
+  @IsNotEmpty()
+  @Field(() => NoticeCategory)
+  noticeCategory: NoticeCategory;
+
+  @IsNotEmpty()
+  @Field(() => NoticeStatus)
+  noticeStatus?: NoticeStatus;
+
+  @IsNotEmpty()
+  @Length(3, 50)
+  @Field(() => String)
+  noticeTitle: string;
+
+  @IsNotEmpty()
+  @Length(3, 250)
+  @Field(() => String)
+  noticeContent: string;
+
+  memberId?: ObjectId;
+}
